5ca2b30f2f4cb47a004b926f766b4de711a9107f
Jay Sandesara <jay.ajitbhai.sandesara@cern.ch>
2026-01-06 05:16:20 +0000

    added the classes for interpolation using ambient fisher

---
+++ ambientfisher/interpolators.py
@@ -0,0 +1,174 @@
+import numpy as np
+import time
+import pandas as pd
+from typing import Union
+import matplotlib.pyplot as plt
+from scipy.spatial import Delaunay
+
+from ambientfisher.utils import barycentric_weights_simplex, \
+    inner_product, choose_base_vertex, l2_normalize, \
+    intrinsic_gnomonic_from_triangle, getChordDistance, \
+    embed_points_on_unit_sphere_from_chord_distances
+
+class AmbientFisherInterpolator:
+    def __init__(self,
+                 anchor_alphas: np.array, 
+                 anchor_pdfs: np.array, 
+                 x_grid: np.ndarray):
+        '''
+        This class of AF is derived from work by Cranmer, Streets and Bandyopadhyay
+        '''
+        self.ambient_dim = len(anchor_alphas[0]) + 1
+        self.sphere_dim = self.ambient_dim - 1
+
+        self.anchor_alphas = anchor_alphas
+        self.anchor_pdfs = list(anchor_pdfs)
+        self.x = x_grid
+
+        # Delauney triangulation
+        self.triangulation = Delaunay(self.anchor_alphas)
+        self.simplices = self.triangulation.simplices
+        self.neighbors = self.triangulation.neighbors
+
+        self.q = []
+        for p in self.anchor_pdfs:
+            q_alpha = np.sqrt(p(self.x))
+            q_alpha = l2_normalize(q_alpha, self.x)
+            self.q.append(q_alpha)
+        
+        self.q = np.array(self.q)
+
+    def enclosing_simplex_indices(self, 
+                     alpha: np.array):
+        simplex_indices = int(self.triangulation.find_simplex(alpha))
+        if simplex_indices==-1:
+            raise Exception("Needs extrapolation")
+        return simplex_indices
+
+    def predict_intrinsic(self, alpha: np.ndarray):
+        '''
+        Predict interpolated PDF at alpha, evaluated on self.x
+        Done intrinsically in Hilbert space - no Euclidean embedding needed
+        '''
+        simplex_indices = self.enclosing_simplex_indices(alpha)
+
+        verts_init      = self.simplices[simplex_indices]
+        anchors         = self.anchor_alphas[verts_init]
+
+        barycentric_weights_init = barycentric_weights_simplex(alpha, anchors)
+
+        simplex_qs = self.q[verts_init]
+
+        inner_product_matrix=[]
+        for f1 in simplex_qs:
+            inner_product_arr = []
+            for f2 in simplex_qs:
+                inner_product_arr.append(inner_product(f1, f2, self.x))
+            inner_product_matrix.append(inner_product_arr)
+        inner_product_matrix=np.array(inner_product_matrix)
+
+        base_vertex = choose_base_vertex(inner_product_matrix)
+
+        simplex_qs_reordered = np.concatenate((simplex_qs[base_vertex:base_vertex+1], 
+                                                simplex_qs[:base_vertex], 
+                                                simplex_qs[base_vertex+1:]))
+
+        barycentric_weights = np.concatenate((barycentric_weights_init[base_vertex:base_vertex+1],
+                                              barycentric_weights_init[:base_vertex],
+                                              barycentric_weights_init[base_vertex+1:]))
+        
+        pdf_alpha = intrinsic_gnomonic_from_triangle(simplex_qs_reordered, 
+                                                     self.x, 
+                                                     barycentric_weights)
+
+        return pdf_alpha
+
+    def predict_extrinsic(self, alpha: np.ndarray, followKyle = True):
+        '''
+        Predict interpolated PDF at alpha, evaluated on self.x
+        Original AF formulation with Euclidean embedding step
+        '''
+        simplex_indices = self.enclosing_simplex_indices(alpha)
+
+        verts_init      = self.simplices[simplex_indices]
+        anchors         = self.anchor_alphas[verts_init]
+
+        barycentric_weights_init          = barycentric_weights_simplex(alpha, anchors)
+
+        simplex_qs = self.q[verts_init]
+
+        inner_product_matrix=[]
+        for f1 in simplex_qs:
+            inner_product_arr = []
+            for f2 in simplex_qs:
+                inner_product_arr.append(inner_product(f1, f2, self.x))
+            inner_product_matrix.append(inner_product_arr)
+        inner_product_matrix=np.array(inner_product_matrix)
+
+        print(np.diag(inner_product_matrix))
+
+        # choose base vertex for stability, then reorder (base first)
+        base_vertex = choose_base_vertex(inner_product_matrix)
+        base_vertex = 0
+
+        simplex_qs_reordered = np.concatenate((simplex_qs[base_vertex:base_vertex+1], 
+                                                simplex_qs[:base_vertex], 
+                                                simplex_qs[base_vertex+1:]))
+
+        barycentric_weights = np.concatenate((barycentric_weights_init[base_vertex:base_vertex+1],
+                                              barycentric_weights_init[:base_vertex],
+                                              barycentric_weights_init[base_vertex+1:]))
+
+        # Step 1: chord distances in Hilbert space
+        chordDistMatrix=[]
+        for f1 in simplex_qs_reordered:
+            chord_distance_arr = []
+            for f2 in simplex_qs_reordered:
+                chord_distance_arr.append(getChordDistance(f1, f2, self.x))
+            chordDistMatrix.append(chord_distance_arr)
+        chordDistMatrix=np.array(chordDistMatrix)
+
+        # Step 2: Euclidean embedding of the N points onto S^N-1 sphere 
+        sphere_embedded_pts = embed_points_on_unit_sphere_from_chord_distances(chordDistMatrix) 
+
+        # Step 3: gnomonic projection to plane z = -1 (south tangent plane) 
+        z = sphere_embedded_pts[:, -1]
+        gnomonic_projection = (-sphere_embedded_pts / z[:, None])     # scale so last coord is -1
+        gnomonic_projection_vertices = gnomonic_projection[:, :self.sphere_dim].copy()
+
+        baryCoords_ = self.triangulation.transform[simplex_indices, :self.sphere_dim].dot(np.transpose(alpha - self.triangulation.transform[simplex_indices, self.sphere_dim]))
+        baryCoords = np.concatenate([baryCoords_, [1.0 - baryCoords_.sum()]])
+
+        for i in range(self.ambient_dim):
+            gnomonicTarget      = baryCoords[i] * gnomonic_projection_vertices[i]
+
+        normedVertices          = gnomonic_projection_vertices.copy()
+        for i in range(1, normedVertices.shape[0]):
+            normedVertices[i] /= np.linalg.norm(normedVertices[i])
+        normedSimplex           = Delaunay(normedVertices)
+        normedBaryCoords_       = normedSimplex.transform[simplex_indices, :self.sphere_dim].dot(np.transpose(gnomonicTarget - normedSimplex.transform[simplex_indices, self.sphere_dim]))
+        normedBaryCoords        = np.concatenate([normedBaryCoords_, [1.0 - normedBaryCoords_.sum()]])
+        t                       = np.arctan(np.linalg.norm(gnomonicTarget))
+
+        # Get tangents on the Hilbert plane
+        tangents_to_vertices_on_plane = []
+        for i in range(1, self.ambient_dim):
+            ci = inner_product(simplex_qs_reordered[0], simplex_qs_reordered[i], self.x)
+            tangents_to_vertices_on_plane.append(simplex_qs_reordered[i] - ci * simplex_qs_reordered[0])
+
+        # Step 4: Find the tangent direction in Hilbert space plane
+        u = []
+        for i in range(self.ambient_dim-1):
+            u.append(l2_normalize(tangents_to_vertices_on_plane[i], self.x))
+        
+        tangent_dir_target = np.zeros_like(simplex_qs_reordered[0])
+        for i in range(self.sphere_dim):
+            tangent_dir_target += u[i] * normedBaryCoords[i]
+
+        tangent_dir_target = l2_normalize(tangent_dir_target, self.x)
+
+        # Step 5: exp map on Hilbert sphere
+        q_hat       = l2_normalize(np.cos(t) * simplex_qs_reordered[0] + np.sin(t) * tangent_dir_target, self.x)
+        p_hat       = q_hat ** 2
+
+        return p_hat
---
+++ ambientfisher/utils.py
@@ -0,0 +1,147 @@
+import numpy as np
+import pandas as pd
+from typing import Union
+import matplotlib.pyplot as plt
+from scipy.spatial import Delaunay
+
+##################################################
+##### Barycentric coordinates 
+##################################################
+
+def barycentric_weights_simplex(alpha, anchors):
+    '''
+    Return the barycentric weights w_i s.t. alpha = \sum_i w_i * a_i and \sum_i w_i = 1
+    '''
+    
+    # Applying \sum_i w_i = 1, w_1 = 1 - w_2 - w_3 
+    # Using this, we form a system of linear equations to solve for w_2 and w_3
+    num_anchors         = anchors.shape[0]
+    M                   = np.column_stack([anchors[i] - anchors[0] for i in range(1, num_anchors)])
+    y                   = alpha - anchors[0]
+    weights_arr         = np.linalg.solve(M, y)
+    sum_weights_arr     = np.sum(weights_arr)
+    w1                  = 1.0 - sum_weights_arr
+    weights_arr         = np.insert(weights_arr, 0, w1)
+    return np.array(weights_arr)
+
+
+#########################################################
+######### Inner Product
+##########################################################
+
+def return_measure(xarray):
+    xmin = np.amin(xarray)
+    xmax = np.amax(xarray)
+    num = len(xarray)
+
+    return (xmax-xmin)/(num-1)
+
+def inner_product(q1, q2, xarray):
+    '''
+    Inner product between two vectors 
+    '''
+    dx = return_measure(xarray)
+
+    if callable(q1) and callable(q2):
+        return np.sum(q1(xarray) * q2(xarray))*dx
+    else:
+        return np.sum(q1 * q2) * dx
+
+def getChordDistance(q1, q2, xarray):
+    return 2.*np.sin( np.arccos( inner_product(q1, q2, xarray) ) /2. )
+
+def l2_normalize(q, xarray):
+    norm_squared = inner_product(q, q, xarray)
+    return q / np.sqrt(norm_squared)
+
+#########################################################
+######### Gnomonic Projection
+##########################################################
+
+def choose_base_vertex(inner_product_matrix):
+    """
+    Choose a base vertex for numerical stability.
+    We pick the vertex that maximizes the *minimum* overlap to the other two.
+    """
+
+    m = inner_product_matrix.shape[0]
+    scores = [np.min(inner_product_matrix[i, np.arange(m)!=i]) for i in range(m)]
+    return int(np.argmax(scores))
+
+#########################################################
+######### Euclidean Embedding for Extrinsic Interpolation
+##########################################################
+def embed_points_on_unit_sphere_from_chord_distances(chord_dist: np.array):
+    """
+    Reconstruct m points on S^{d-1} given chord distances. 
+    Cosmetically modified code of the original in https://github.com/anjishnu1991/interpolation/tree/master
+    """
+    m = chord_dist.shape[0]
+    sphere_dim = m - 1
+
+    pts = np.zeros((m, m), dtype=float)
+    pts[0, -1] = -1.0  # south pole in last coordinate
+
+    for k in range(1, m):
+        M = np.empty((k, k), dtype=float)
+        y = np.zeros(k, dtype=float)
+
+        for j in range(k):
+            d = chord_dist[j, k]
+            y[j] = 1.0 - 0.5 * d ** 2      # dot product computation
+            M[j, :] = pts[j, -k:]         # last k coordinates of previous point
+
+        tail = np.linalg.solve(M, y)
+        rem_sq = 1.0 - np.dot(tail, tail)
+        first = -np.sqrt(rem_sq)        # choose southern hemisphere
+
+        active = np.concatenate([[first], tail])      # length k+1
+        pts[k, :] = np.concatenate([np.zeros(sphere_dim - k), active])
+
+    return pts
+
+#########################################################
+######### Building the interpolant
+##########################################################
+
+def intrinsic_gnomonic_from_triangle(q_array, xarray, barycentric_weights):
+    '''
+    Given unit-norm sqrt densities q1, q2, q3 and barycentric weights for the corresponding alpha, 
+    with q1 mapped to 0 in its own tangent place, the function returns interpolated pdf at alpha.
+
+    This is the direct Hilber-space modification to the original AF.
+
+    Gnomonic projection formula in the intrinsic space: 
+    g_i = (q_i - <q1, q_i> q1) / <q1, q_i>
+
+    And the interpolation is done using the standard AF formula:
+    g = w2*g2 + w3*g3
+    q(alpha) = normalize(q1 + g)
+    p(alpha) = q(alpha)**2
+    '''
+
+    gnomonic_projections = []
+    anchor_length = q_array.shape[0]
+
+    for i in range(1, anchor_length):
+
+        c_i0 = inner_product(q_array[0], q_array[i], xarray)
+        if callable(q_array[0]):
+            gnomonic_projections.append((q_array[i](xarray) - c_i0 * q_array[0](xarray)) / c_i0)
+        else:
+            gnomonic_projections.append((q_array[i] - c_i0 * q_array[0]) / c_i0)
+
+    gnomonic_projections =  np.array(gnomonic_projections)
+
+    g = np.dot(barycentric_weights[1:], gnomonic_projections)
+
+    if callable(q_array[0]):
+        q_alpha = q_array[0](xarray) + g
+    else:
+        q_alpha = q_array[0] + g
+
+    q_alpha = l2_normalize(q_alpha, xarray)
+
+    p_alpha = q_alpha**2
+    
+    return p_alpha
